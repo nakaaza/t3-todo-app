f575a504b0f46a71cd65ef9a62924944a827d99a
Taichi Nakaza <nakaaza@Taichis-MacBook-Air.local>
2024-02-12 00:00:42 +0900

    commit for deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { signOut, useSession } from "next-auth/react";
 import { Auth } from "../components/Auth";
 import { Layout } from "../components/Layout";
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import { TaskForm } from "../components/TaskForm";
+import { TaskList } from "../components/TaskList";
 
 const Home: NextPage = () => {
 	const { data: session } = useSession();
@@ -21,6 +23,8 @@ const Home: NextPage = () => {
 			onClick={() => signOut()}
 		/>
 		<p className="my-3 text-xl text-blue-600">{session?.user?.name}</p>
+		<TaskForm />
+		<TaskList />
 	 </Layout>
   );
 };
